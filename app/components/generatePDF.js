52af9195be2ba40cd126da920e4cb026e95fca4d
Md. Borhan Hosen <borhanhosen3@gmail.com>
2025-01-29 01:57:39 +0600

    update header and footer in pdf

--- app/components/generatePDF.js
+++ app/components/generatePDF.js
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
   pageNumber: {
     position: "absolute",
     fontSize: 12,
-    bottom: 30,
+    bottom: 20,
     left: 0,
     right: 0,
     textAlign: "center",
